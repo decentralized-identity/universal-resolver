22cc6c5eb47160c919a60c7a6c30d315f8373767
Markus Sabadello <markus@danubetech.com>
2017-08-21 17:56:23 +0400

    public URL for the example code

--- examples/src/main/java/uniresolver/examples/TestClientUniResolver.java
+++ examples/src/main/java/uniresolver/examples/TestClientUniResolver.java
@@ -7,7 +7,7 @@ public class TestClientUniResolver {
 	public static void main(String[] args) throws Exception {
 
 		ClientUniResolver uniResolver = new ClientUniResolver();
-		uniResolver.setResolverUri("http://localhost:8080/1.0/identifiers/");
+		uniResolver.setResolverUri("https://uniresolver.danubetech.com/1.0/identifiers/");
 
 		DDO ddo1 = uniResolver.resolve("did:sov:AdLi7vX2z1bLyVZaoy18K1");
 		System.out.println(ddo1.serialize());
