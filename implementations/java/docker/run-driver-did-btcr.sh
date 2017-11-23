3b7067d92abbcead1744b4631d4b39e493f80857
Markus Sabadello <markus@danubetech.com>
2017-11-23 09:22:30 +0100

    use /bin/sh instead of /bin/bash

--- implementations/java/docker/run-driver-did-btcr.sh
+++ implementations/java/docker/run-driver-did-btcr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 cd /opt/uni-resolver-java/driver-did-btcr/
 mvn jetty:run -P war
--- implementations/java/docker/run-driver-did-stack.sh
+++ implementations/java/docker/run-driver-did-stack.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 cd /opt/uni-resolver-java/driver-did-stack/
 mvn jetty:run -P war
