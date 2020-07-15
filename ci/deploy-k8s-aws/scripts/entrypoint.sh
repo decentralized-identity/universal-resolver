f3efe0147eecd68ef9edb9ead1a64f0f8b9e4581
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-07-15 16:43:47 +0200

    fix: copy script files to current folder

--- ci/deploy-k8s-aws/scripts/entrypoint.sh
+++ ci/deploy-k8s-aws/scripts/entrypoint.sh
@@ -9,7 +9,7 @@ ls -al
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-#cp /prepare-deployment.py /k8s-template.yaml . 2>/dev/null || :
+cp /prepare-deployment.py /k8s-template.yaml .
 
 python --version
 python prepare-deployment.py
