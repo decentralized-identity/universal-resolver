9ff41274006b51adfb37a4a1fb39c04a35b96c07
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-07-15 15:25:44 +0200

    fix: upgrade convert.py script to add all deployment to deploy.sh

--- ci/deploy-k8s-aws/scripts/convert.py
+++ ci/deploy-k8s-aws/scripts/convert.py
@@ -5,81 +5,83 @@ import os
 import getopt
 import yaml
 import subprocess
+from shutil import copy
 
 # CONSTANTS you may need to change:
 DEFAULT_DOMAIN_NAME = 'dev.uniresolver.io'
 UNIVERSAL_RESOLVER_FRONTEND_TAG = "universalresolver/uni-resolver-frontend:latest;"
 
 
-def initDeploymentDir(outputdir):
-#  if os.path.exists(outputdir + '/' + 'deploy.sh'):
-#    os.remove(outputdir + '/' +'deploy.sh')
-#  if not os.path.exists(outputdir):
-#    os.makedirs(outputdir)
-#  fout = open(outputdir + '/' + 'deploy.sh', "a+")
-#  fout.write('kubectl delete all --all -n uni-resolver\n')
-#  fout.close()
-  subprocess.call(['chmod', "a+x", outputdir + '/' + 'deploy.sh'])
+def init_deployment_dir(outputdir):
+    if os.path.exists(outputdir + '/' + 'deploy.sh'):
+        os.remove(outputdir + '/' + 'deploy.sh')
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    fout = open(outputdir + '/' + 'deploy.sh', "a+")
+    fout.write('kubectl delete all --all -n uni-resolver\n')
+    fout.write('./namespace-setup.sh\n')
+    fout.close()
+    subprocess.call(['chmod', "a+x", outputdir + '/' + 'deploy.sh'])
 
-def addDeployment(containerName, deploymentFile, outputdir):
-   fout = open(outputdir + '/' + 'deploy.sh', "a+")
-   fout.write('kubectl -n uni-resolver apply -f %s \n'%deploymentFile)
-   fout.close()
 
-def getContainerNameVersion(containterTag):
-  if (containterTag.find('/') < 0):
-    return
-  user,containerNameVersion = containterTag.split('/')
-  return containerNameVersion.split(':')
+def add_deployment(deployment_file, outputdir):
+    fout = open(outputdir + '/' + 'deploy.sh', "a+")
+    fout.write('kubectl apply -n uni-resolver -f %s \n' % deployment_file)
+    fout.close()
 
-def generateDeploymentSpecs(containterTags, outputdir):
-    containerPort = '8080'
-    for containterTag in containterTags.split(';'):
-        if (containterTag == ''):
-          return
-        containerName, containerVersion = getContainerNameVersion(containterTag)
+
+def get_container_name_version(container_tag):
+    if container_tag.find('/') < 0:
+        return
+    user, container_name_version = container_tag.split('/')
+    return container_name_version.split(':')
+
+
+def generate_deployment_specs(container_tags, outputdir):
+    for container_tag in container_tags.split(';'):
+        if container_tag == '':
+            return
+        container_name, container_version = get_container_name_version(container_tag)
         fin = open("k8s-template.yaml", "rt")
-        deploymentFile = "deployment-%s.yaml" % containerName
-        if (containterTag == UNIVERSAL_RESOLVER_FRONTEND_TAG.replace(';','')):
-          containerPort = '80'
-        f = outputdir + '/' + deploymentFile
-        if (os.path.isfile(f) == False):
-          fout = open(outputdir + '/' + deploymentFile, "wt")
-          print('Writing file: ' + outputdir + '/' + deploymentFile + ' for containter: ' + containterTag)
-          for line in fin:
-              fout.write(line.replace('{{containerName}}', containerName).replace('{{containterTag}}', containterTag).replace('{{containerPort}}', containerPort))
-          #addDeployment(containerName, deploymentFile, outputdir)
-          fout.close()
+        deployment_file = "deployment-%s.yaml" % container_name
+        fout = open(outputdir + '/' + deployment_file, "wt")
+        print('Writing file: ' + outputdir + '/' + deployment_file + ' for container: ' + container_tag)
+        for line in fin:
+            fout.write(line.replace('{{containerName}}', container_name).replace('{{containerTag}}', container_tag))
+        add_deployment(deployment_file, outputdir)
         fin.close()
+        fout.close()
 
-def findInDir(key, dictionary):
+
+def find_in_dir(key, dictionary):
     for k, v in dictionary.items():
         if k == key:
             yield v
         elif isinstance(v, dict):
-            for result in findInDir(key, v):
+            for result in find_in_dir(key, v):
                 yield result
 
-def getContainerTags(fileName):
+
+def get_container_tags(fileName):
     with open(fileName, 'r') as file:
         yaml_str = file.read()
         data = yaml.safe_load(yaml_str)
 
-    containerTags = ''
-    for x in findInDir("image", data):
-        containerTags += x + ';'
+    container_tags = ''
+    for x in find_in_dir("image", data):
+        container_tags += x + ';'
 
-    return containerTags
-    
+    return container_tags
 
-def generateIngress(containterTags, outputdir):
+
+def generate_ingress(container_tags, outputdir):
     global DEFAULT_DOMAIN_NAME
     print("Generating uni-resolver-ingress.yaml")
     fout = open(outputdir + '/uni-resolver-ingress.yaml', "wt")
     fout.write('apiVersion: extensions/v1beta1\n')
     fout.write('kind: Ingress\n')
     fout.write('metadata:\n')
-    fout.write('  name: \"uni-resolver-web\"\n')
+    fout.write('  name: \"uni-resolver-ingress\"\n')
     fout.write('  namespace: \"uni-resolver\"\n')
     fout.write('  annotations:\n')
     fout.write('    kubernetes.io/ingress.class: alb\n')
@@ -101,58 +103,74 @@ def generateIngress(containterTags, outputdir):
     fout.write('          - path: /*\n')
     fout.write('            backend:\n')
     fout.write('              serviceName: "uni-resolver-frontend"\n')
-    fout.write('              servicePort: 80\n')
+    fout.write('              servicePort: 8080\n')
 
-    for containterTag in containterTags.split(';'):
-      if (containterTag == ''):
-        return
-      containerName, containerVersion = getContainerNameVersion(containterTag)
-      if (containerName == 'uni-resolver-web'): # this is the default-name, hosted at: DEFAULT_DOMAIN_NAME
-        continue
-      subDomainName = containerName.replace('did', '').replace('driver', '').replace('uni-resolver', '').replace('-', '')
-      print('Adding domain: ' + subDomainName + '.' + DEFAULT_DOMAIN_NAME)
+    for container_tag in container_tags.split(';'):
+        if container_tag == '':
+            return
+        container_name, container_version = get_container_name_version(container_tag)
+        if container_name == 'uni-resolver-web':  # this is the default-name, hosted at: DEFAULT_DOMAIN_NAME
+            continue
+        sub_domain_name = container_name.replace('did', '').replace('driver', '').replace('uni-resolver', '').replace('-',
+                                                                                                                   '')
+        print('Adding domain: ' + sub_domain_name + '.' + DEFAULT_DOMAIN_NAME)
 
-      fout.write('    - host: ' + subDomainName + '.' + DEFAULT_DOMAIN_NAME + '\n')
-      fout.write('      http:\n')
-      fout.write('        paths:\n')
-      fout.write('          - path: /*\n')
-      fout.write('            backend:\n')
-      fout.write('              serviceName: "' + containerName + '"\n')
-      fout.write('              servicePort: 8080\n')
+        fout.write('    - host: ' + sub_domain_name + '.' + DEFAULT_DOMAIN_NAME + '\n')
+        fout.write('      http:\n')
+        fout.write('        paths:\n')
+        fout.write('          - path: /*\n')
+        fout.write('            backend:\n')
+        fout.write('              serviceName: "' + container_name + '"\n')
+        fout.write('              servicePort: 8080\n')
 
     fout.close()
 
+
+def copy_app_deployment_specs():
+    copy('./app-specs/deployment-uni-resolver-frontend.yaml', './out/deployment-uni-resolver-frontend.yaml')
+    add_deployment('deployment-uni-resolver-frontend.yaml', './out')
+    copy('./app-specs/deployment-uni-resolver-web.yaml', './out/deployment-uni-resolver-web.yaml')
+    add_deployment('deployment-uni-resolver-web.yaml', './out')
+
+
 def main(argv):
-   compose = 'docker-compose.yml'
-   outputdir = './deploy'
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["compose=","outputdir="])
-   except getopt.GetoptError:
-      print('./convert.py -i <inputfile> -o <outputdir>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('./convert.py -i <inputfile> -o <outputdir>')
-         sys.exit()
-      elif opt in ("-i", "--compose"):
-         compose = arg
-      elif opt in ("-o", "--outputdir"):
-         outputdir = arg
+    compose = 'docker-compose.yml'
+    outputdir = './out'
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["compose=", "outputdir="])
+    except getopt.GetoptError:
+        print('./convert.py -i <inputfile> -o <outputdir>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('./convert.py -i <inputfile> -o <outputdir>')
+            sys.exit()
+        elif opt in ("-i", "--compose"):
+            compose = arg
+        elif opt in ("-o", "--outputdir"):
+            outputdir = arg
 
-   print ('Input file is:', compose)
-   print ('Output dir is:', outputdir)
+    print('Input file is:', compose)
+    print('Output dir is:', outputdir)
 
-   initDeploymentDir(outputdir)
+    init_deployment_dir(outputdir)
 
-   containerTags = getContainerTags(compose)
-   print("Container tags: " + containerTags)
+    container_tags = get_container_tags(compose)
+    print("Container tags: " + container_tags)
 
-   #generateIngress(containerTags, outputdir)
+    generate_ingress(container_tags, outputdir)
 
-   # we need also a spec for the frontend
-   containerTags += UNIVERSAL_RESOLVER_FRONTEND_TAG
-   generateDeploymentSpecs(containerTags, outputdir)
+    # generate driver specs
+    generate_deployment_specs(container_tags, outputdir)
+
+    # copy app deployment specs
+    copy_app_deployment_specs()
+
+    # copy namespace files
+    copy('./namespace-setup.yaml', './deploy/namespace-setup.yaml')
+    copy('./namespace-setup.sh', './deploy/namespace-setup.sh')
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
+    print('TODO manually: change uni-resolver-frontend ingress port to 80!!')
