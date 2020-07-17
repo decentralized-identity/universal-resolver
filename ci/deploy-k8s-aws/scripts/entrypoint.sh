28e74e7e4edb06973acd245659d93c7c43d40918
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-07-17 12:58:01 +0200

    fix: wrong port for uport and trustbloc

--- ci/deploy-k8s-aws/scripts/entrypoint.sh
+++ ci/deploy-k8s-aws/scripts/entrypoint.sh
@@ -4,26 +4,24 @@ echo "#### Kubernetes Deployment of the Universal Resolver ####"
 
 set -e
 
-echo "Root folder"
+echo "## Root folder ##"
 ls -al /
 
 ls -al
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-cp /prepare-deployment.py /k8s-template.yaml -r /app-specs -r /namespace . 2>/dev/null || :
-
-echo "Current workspace Folder"
+echo "### Prepare deployment ###"
 pwd
 ls -al .
 python --version
 python prepare-deployment.py
 
-echo "Deployment Folder"
+echo "## Deployment Folder ##"
 cd deploy
 ls -al .
 
-echo "### Deploying following Specs:"
+echo "### Deploying following Specs ### "
 cat deploy.sh
 
 ./deploy.sh
