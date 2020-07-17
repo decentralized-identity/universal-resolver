a50e93e6f2d2d815e041978e9c528ed845cc2655
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-07-17 13:56:52 +0200

    fix: re-add copy part in entrypoint.sh

--- ci/deploy-k8s-aws/scripts/entrypoint.sh
+++ ci/deploy-k8s-aws/scripts/entrypoint.sh
@@ -10,7 +10,9 @@ ls -al /
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-echo "### Prepare deployment ###"
+cp /prepare-deployment.py /k8s-template.yaml -r /app-specs -r /namespace . 2>/dev/null || :
+
+echo "Current workspace Folder"
 pwd
 ls -al .
 python --version
