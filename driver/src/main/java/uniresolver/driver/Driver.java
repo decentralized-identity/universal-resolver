745bf6ff2b486fe0355521bca7c422bfc7af46a9
Markus Sabadello <markus@danubetech.com>
2021-03-30 15:10:10 +0200

    fix: Make properties() and testIdentifiers() methods optional.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- driver/src/main/java/uniresolver/driver/Driver.java
+++ driver/src/main/java/uniresolver/driver/Driver.java
@@ -1,5 +1,6 @@
 package uniresolver.driver;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +12,14 @@ public interface Driver {
 	public static final String PROPERTIES_MIME_TYPE = "application/json";
 
 	public ResolveResult resolve(String identifier) throws ResolutionException;
-	public Map<String, Object> properties() throws ResolutionException;
-	public List<String> testIdentifiers() throws ResolutionException;
+
+	default public Map<String, Object> properties() throws ResolutionException {
+
+		return Collections.emptyMap();
+	}
+
+	default public List<String> testIdentifiers() throws ResolutionException {
+
+		return Collections.emptyList();
+	}
 }
