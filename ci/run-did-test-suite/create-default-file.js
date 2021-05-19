3f12b80a9d4b78870035efce4421acf233e376bf
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-05-19 12:21:41 +0200

    Use testsuite folder and recreate default.js file

--- ci/run-did-test-suite/create-default-file.js
+++ ci/run-did-test-suite/create-default-file.js
@@ -1,25 +1,21 @@
 const fs = require("fs")
 
-const resolvers = []
 
-fs.readdir('./results', (err, files) => {
+fs.readdir('/Users/devfox/testsuites/did-test-suite/packages/did-core-test-server/suites/implementations', (err, files) => {
+
+    const resolvers = []
+
     files.forEach(file => {
         console.log(file);
         resolvers.push(`require('../implementations/${file}')`)
     });
     console.log(resolvers)
 
-    const defaultFile = require('/Users/devfox/testsuites/did-test-suite/packages/did-core-test-server/suites/did-resolution/default.js');
-    console.log(defaultFile)
-
-    defaultFile.resolvers = resolvers
-    console.log(defaultFile)
-
     fs.writeFileSync(
         '/Users/devfox/testsuites/did-test-suite/packages/did-core-test-server/suites/did-resolution/default.js',
         `module.exports = {
-            name: ${JSON.stringify(defaultFile.name)},
-            resolvers: [${defaultFile.resolvers}] 
+            name: '7.1 DID Resolution',
+            resolvers: [${resolvers}] 
         }`
     );
 });
