7d0db53139879885566c8d68f1854936923f5a46
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-05-20 13:03:54 +0200

    Include only resolver related files

--- ci/run-did-test-suite/create-default-file.js
+++ ci/run-did-test-suite/create-default-file.js
@@ -1,13 +1,17 @@
 const fs = require("fs")
 
-
-fs.readdir('/Users/devfox/testsuites/did-test-suite/packages/did-core-test-server/suites/implementations', (err, files) => {
+const path = '/Users/devfox/testsuites/did-test-suite/packages/did-core-test-server/suites/implementations';
+fs.readdir(path, (err, files) => {
 
     const resolvers = []
 
     files.forEach(file => {
         console.log(file);
-        resolvers.push(`require('../implementations/${file}')`)
+        const fileContent = JSON.parse(fs.readFileSync(`${path}/${file}`));
+        console.log(fileContent);
+        if (file.startsWith('universal-resolver') || file.startsWith('resolver')) {
+            resolvers.push(`require('../implementations/${file}')`)
+        }
     });
     console.log(resolvers)
 
@@ -15,7 +19,7 @@ fs.readdir('/Users/devfox/testsuites/did-test-suite/packages/did-core-test-serve
         '/Users/devfox/testsuites/did-test-suite/packages/did-core-test-server/suites/did-resolution/default.js',
         `module.exports = {
             name: '7.1 DID Resolution',
-            resolvers: [${resolvers}] 
+            resolvers: [${resolvers}]
         }`
     );
 });
