64bdf3f289dbdf9169d78ffc64de3141da2523aa
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-05-25 18:48:10 +0200

    Add git status logs

--- ci/run-did-testsuite/entrypoint.sh
+++ ci/run-did-testsuite/entrypoint.sh
@@ -11,7 +11,8 @@ echo "driver_status_report: $INPUT_DRIVER_STATUS_REPORT"
 node /run-did-testsuite/index.js --HOST="$INPUT_HOST" --DRIVER_STATUS_REPORT="$INPUT_DRIVER_STATUS_REPORT" --OUTPUT_PATH="$INPUT_REPORTS_FOLDER"
 
 echo "Push report file to repo"
+git status
 git config --global user.email "admin@danubetech.com"
 git config --global user.name "Get driver status workflow"
-git fetch && git pull
+git fetch && git pull --ff-only
 git add . && git commit -m "Did testsuite report" && git push
