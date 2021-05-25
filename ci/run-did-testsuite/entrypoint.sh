0bf4010666e416c1cfa874c5bac44d028225104c
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-05-25 16:24:19 +0200

    Fix path to action in entrypoint.sh

--- ci/run-did-testsuite/entrypoint.sh
+++ ci/run-did-testsuite/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "#### Run did did testsuite ####"
+echo "#### Run did testsuite ####"
 
 echo "Running with parameters:"
 sh -c "echo $*"
@@ -8,7 +8,7 @@ sh -c "echo $*"
 echo "host: $INPUT_HOST"
 echo "driver_status_report: $INPUT_DRIVER_STATUS_REPORT"
 
-node index.js --HOST="$INPUT_HOST" --DRIVER_STATUS_REPORT="$INPUT_DRIVER_STATUS_REPORT" --OUTPUT_PATH="$INPUT_REPORTS_FOLDER"
+node /run-did-testsuite/index.js --HOST="$INPUT_HOST" --DRIVER_STATUS_REPORT="$INPUT_DRIVER_STATUS_REPORT" --OUTPUT_PATH="$INPUT_REPORTS_FOLDER"
 
 echo "Push report file to repo"
 git config --global user.email "admin@danubetech.com"
