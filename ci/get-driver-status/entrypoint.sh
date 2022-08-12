e749d892ee554e03158d9cb48bb407adb9d1f9c8
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2022-08-12 17:19:24 +0200

    Fix issue with creating output path (#314)
    * ci: migrate testscript to yml
    
    * fix: run tests on main and push results to driver-status-reports
    
    * fix: use correct path to config file
    
    * ci: use dev branch to run tests
    
    * fix: add driver-status-reports folder
    
    * ci: fix default config path for driver-status-action
    
    * fix: use correct source branch
    
    * fix: switch to branch before commit
    
    * ci: pump version for Dockerfile
    
    * fix: use git checkout instead of git switch
    
    * fix: wrong variable name for out parameter
    
    * chore: print git version
    
    * fix: use git checkout because git version is too old for switch
    
    * fix: typo with branch name
    
    * fix: do git add before checkout of driver-status-reports branch
    
    * fix: use correct out folder path
    
    * fix: use correct variable name for out in shell script
    
    * fix: hardcode out path for debugging
    
    * fix: add debugging lines
    
    * fix: add -p to makedir to also create parent folders
    
    * fix: use input parameter for REPORTS_FOLDER
    
    * fix: use git add with wildcard and remove debug logs
    
    * fix: create local branch during checkout

--- ci/get-driver-status/entrypoint.sh
+++ ci/get-driver-status/entrypoint.sh
@@ -7,7 +7,7 @@ sh -c "echo $*"
 
 echo "host: $INPUT_HOST"
 echo "config: $INPUT_CONFIG"
-echo "out folder: $INPUT_OUT_FOLDER"
+echo "out folder: $INPUT_OUT"
 echo "debug: $INPUT_DEBUG"
 echo "keep result: $INPUT_KEEP_RESULT"
 
@@ -27,27 +27,25 @@ if "$INPUT_DEBUG"; then
 fi
 
 DATE_WITH_TIME=$(TZ=UTC date "+%Y-%m-%d_%H:%M:%S")
-REPORTS_FOLDER="$INPUT_OUT_FOLDER/nightly-run-$DATE_WITH_TIME"
-mkdir "$REPORTS_FOLDER"
+REPORTS_FOLDER="$INPUT_OUT/nightly-run-$DATE_WITH_TIME"
+mkdir -p "$REPORTS_FOLDER"
 
 python --version
 python /get-driver-status/get-driver-status.py --host "$INPUT_HOST" --config "$INPUT_CONFIG" --out "$REPORTS_FOLDER"
 
 echo "Switch to drivers-status-reports branch"
-git version
 git fetch
-git checkout driver-status-reports
-git status
+git checkout -b driver-status-reports origin/driver-status-reports
 
 if "$INPUT_KEEP_RESULT";
   then
     echo "Push result file to repo"
     git config --global user.email "admin@danubetech.com"
     git config --global user.name "Get driver status workflow"
-    git add .
     # Pass driver_status_report to next step in github action
     echo "driver_status_report=$(git diff --name-only --staged)" >> "$GITHUB_ENV"
     echo "reports_folder=$REPORTS_FOLDER" >> "$GITHUB_ENV"
+    git add .
     git commit -m "Get driver status results"
     git push origin driver-status-reports:driver-status-reports
   else
