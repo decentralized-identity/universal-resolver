66081e1970430955cc79107e90a4c2a6de6777da
BernhardFuchs <bernhard.fuchs@danubetech.com>
2023-03-06 12:59:18 +0100

    chore: checkout to did-lint-reports branch before commit and push

--- ci/did-lint-check/run.sh
+++ ci/did-lint-check/run.sh
@@ -6,10 +6,13 @@ docker pull oydeu/did-status-generator:latest
 echo "Running did-status-generator image"
 docker run --rm oydeu/did-status-generator:latest > result.json
 
+echo "Checkout to did-lint-reports branch"
+git fetch
+git checkout -b did-lint-reports origin/did-lint-reports
+
 echo "Push result file to repo"
 git config --global user.email "admin@danubetech.com"
 git config --global user.name "DID Lint check workflow"
-git fetch
 git add result.json
 git commit -m "DID Lint check reports"
 git push origin did-lint-reports:did-lint-reports
