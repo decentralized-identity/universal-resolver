02feb81812b794a6842ff7214d4886fdf5a6b290
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2023-03-13 18:56:08 +0100

    Bernhard fuchs fix did lint workflow (#360)
    * chore: add git status for debuggin
    
    * chore: add more debug logs
    
    * chore: add logs before switching to branch
    
    * fix: use result_var to move results from main to did-lint-reports
    
    * fix: reference variable

--- ci/did-lint-check/run.sh
+++ ci/did-lint-check/run.sh
@@ -5,14 +5,20 @@ docker pull oydeu/did-status-generator:latest
 
 echo "Running did-status-generator image"
 docker run --rm oydeu/did-status-generator:latest > result.json
+echo "Set result.json to result_var"
+result_var=$(cat result.json)
 
 echo "Checkout to did-lint-reports branch"
 git fetch
 git switch did-lint-reports --force
 
+echo "Replace old result.json with result_var"
+echo "$result_var" > result.json
+
 echo "Push result file to repo"
 git config --global user.email "admin@danubetech.com"
 git config --global user.name "DID Lint check workflow"
+git status
 git add result.json
 git commit -m "DID Lint check reports"
 git push origin did-lint-reports:did-lint-reports
