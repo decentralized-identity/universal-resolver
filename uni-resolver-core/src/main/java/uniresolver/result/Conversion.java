1141448ff4659ee10e36b7ac5d7e59cd332a14a5
Markus Sabadello <markus@danubetech.com>
2023-08-14 23:58:58 +0400

    chore: Reduce conversion log level to trace.

--- uni-resolver-core/src/main/java/uniresolver/result/Conversion.java
+++ uni-resolver-core/src/main/java/uniresolver/result/Conversion.java
@@ -68,7 +68,7 @@ class Conversion {
 
         // done
 
-        if (log.isDebugEnabled()) log.debug("Converted to resolve() result using consumer " + (representationConsumer == null ? null : representationConsumer.getClass()) + ": " + resolveRepresentationResult + " --------> " + resolveDataModelResult);
+        if (log.isTraceEnabled()) log.trace("Converted to resolve() result using consumer " + (representationConsumer == null ? null : representationConsumer.getClass()) + ": " + resolveRepresentationResult + " --------> " + resolveDataModelResult);
         return resolveDataModelResult;
     }
 
@@ -122,7 +122,7 @@ class Conversion {
 
         // done
 
-        if (log.isDebugEnabled()) log.debug("Converted to resolveRepresentation() result using producer " + (representationProducer == null ? null : representationProducer.getClass()) + ": " + resolveDataModelResult + " --------> " + resolveRepresentationResult);
+        if (log.isTraceEnabled()) log.trace("Converted to resolveRepresentation() result using producer " + (representationProducer == null ? null : representationProducer.getClass()) + ": " + resolveDataModelResult + " --------> " + resolveRepresentationResult);
         return resolveRepresentationResult;
     }
 }
