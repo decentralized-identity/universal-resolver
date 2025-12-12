653d58dac4966d940e260aa61a2e800b62a48422
Markus Sabadello <markus@danubetech.com>
2025-12-12 10:16:20 +0100

    Optimize imports

--- uni-resolver-local/src/main/java/uniresolver/local/LocalUniResolver.java
+++ uni-resolver-local/src/main/java/uniresolver/local/LocalUniResolver.java
@@ -1,7 +1,6 @@
 package uniresolver.local;
 
 import foundation.identity.did.DID;
-import foundation.identity.did.DIDURL;
 import foundation.identity.did.parser.ParserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
